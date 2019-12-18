d45e3c850ae5854dd7a86a63eea79baa0ad6fab4
zerkyozero69 <mai_lineage2@hotmail.com>
2019-12-18 15:36:31 +0700

    การส่ง-รับเสบียงสัตว์

---
+++ WebApi.Jwt/Controllers/แผนการผลิต/PlanSeedInfo_SumController.cs
@@ -0,0 +1,35 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using nutrition.Module;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using WebApi.Jwt.Models;
+
+namespace WebApi.Jwt.Controllers.แผนการผลิต
+{
+    public class PlanSeedInfo_SumController : ApiController
+    {
+        string scc = ConfigurationManager.ConnectionStrings["scc"].ConnectionString.ToString();
+
+        public HttpResponseMessage PlanSeedInfo_Detail()
+        {
+            try
+            {
+                XpoTypesInfoHelper.GetXpoTypeInfoSource();
+                XafTypesInfo.Instance.RegisterEntity(typeof(PlanSeedInfo));
+                XPObjectSpaceProvider directProvider = new XPObjectSpaceProvider(scc, null);
+                IObjectSpace ObjectSpace = directProvider.CreateObjectSpace();
+                IList<SendOrderSeed> collection = ObjectSpace.GetObjects<SendOrderSeed>(CriteriaOperator.Parse("GCRecord is null and SendStatus=2 and SendOrgOid.Oid='" + org_oid + "'", null));
+                if (collection.Count > 0)
+
+            }
+        }
+    }
+}
